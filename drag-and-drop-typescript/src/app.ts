f84356d8f1e5f5dff71dc9c63d1f314b78d82ca7
Nithin Samuel <nithin.thewebdev@gmail.com>
2021-06-01 12:05:21 +0530

    added drag and drop code

--- drag-and-drop-typescript/src/app.ts
+++ drag-and-drop-typescript/src/app.ts
@@ -1,4 +1,17 @@
 // Code goes here!
+// Drag and Drop Interfaces
+//we need 2 event handlers
+interface Draggable{
+    dragStartHandler(event:DragEvent):void;
+    dragEndHandler(event:DragEvent):void;
+}
+//three event handlers are required
+interface DragTarget{
+    dragOverHandler(event:DragEvent):void;
+    dropHandler(event:DragEvent):void;
+    dragLeaveHandler(event:DragEvent):void;
+}
+
 enum ProjectStatus {Active, Finsihed}
 // custom type using class so we can instantiate it
 /***Project class ***/
@@ -159,8 +172,19 @@ abstract class Component<T extends HTMLElement,U extends HTMLElement>{
 //***ProjectItem Class ***/ 
 //set generic types for base class we are inheriting
 //we will use this class to generate the individual the project list items
-class ProjectItem extends Component<HTMLUListElement,HTMLLIElement>{
+// when we implement the implements Draggable interface it forces us to implement two methods
+class ProjectItem extends Component<HTMLUListElement,HTMLLIElement> implements Draggable{
     private project:Project;
+    // getter method to modify the text based on the number of people (i.e Persons or Person)
+    // getter is accessed like a normal property (i.e this.persons)    
+    get persons(){
+        if(this.project.people===1){
+            return '1 person'
+        }else{
+            return `${this.project.people} persons`
+        }
+    }
+
     //the id is not fixed it can be either active or finished 
     //1)hostId -we need to provide the id of the element where the project items should be rendered
     //2)project-data that will be rendered-we receive it in object form
@@ -173,20 +197,38 @@ class ProjectItem extends Component<HTMLUListElement,HTMLLIElement>{
         this.configure();
         this.renderContent();
     }  
-    configure(){
+    //interface Draggable methods
+    dragStartHandler(event:DragEvent){
+        console.log("dragStartHandler--event",event);
+        // attach data to the drag event (we save the project id)
+        event.dataTransfer!.setData("text/plain",this.project.id);
+        // cursor appearance
+        event.dataTransfer!.effectAllowed='move';
+    }
 
+    dragEndHandler(_:DragEvent){
+        console.log("DragEnd");
+    }
+
+    //abstract methods
+    configure(){
+        // "this" keyword must point to this class
+        this.element.addEventListener('dragstart',this.dragStartHandler.bind(this));
+        this.element.addEventListener('dragend',this.dragEndHandler.bind(this));
     }  
     renderContent(){
         // reach out to the elements in renderContent 
         this.element.querySelector('h2')!.textContent=this.project.title;
-        this.element.querySelector('h3')!.textContent=this.project.people.toString();
+        // this.element.querySelector('h3')!.textContent=this.project.people.toString()+' assigned';
+        //using getter
+        this.element.querySelector('h3')!.textContent=this.persons+' assigned';
         this.element.querySelector('p')!.textContent=this.project.description;
     }
 }
 
 //***ProjectList Class***/
 //set generic types for base class we are inheriting-<HTMLDivElement,HTMLElement>
-class ProjectList extends Component<HTMLDivElement,HTMLElement>{   
+class ProjectList extends Component<HTMLDivElement,HTMLElement> implements DragTarget{   
     assignedProjects:Project[];
     // the type of the project should be 'active'|'finished'
     // we must get this type when the project is instantiated
@@ -201,8 +243,38 @@ class ProjectList extends Component<HTMLDivElement,HTMLElement>{
         // render list to dom()       
         this.renderContent();
     }
-     // implement abstract method
+    // implememt DragTarget interface methods
+    //fires when we drag a valid draggable target
+    @autobind
+    dragOverHandler(event:DragEvent){
+        //is data attached to our drag event of the format 
+        //this is the type we set in dragStartHandler in ProjectItem class 
+        if(event.dataTransfer && event.dataTransfer.types[0]==="text/plain"){
+            //default for js events is to prevent drops, so we need to execute the event.preventDefault();
+            event.preventDefault();
+            const listEl=this.element.querySelector('ul');
+            listEl?.classList.add('droppable');
+        }
+       
+    }
+    dropHandler(event:DragEvent){
+        console.log("dropHandler-event",event.dataTransfer!.getData('text/plain'));
+    }
+    //when we leave this element with a dragged element
+    @autobind
+    dragLeaveHandler(_:DragEvent){
+        const listEl=this.element.querySelector('ul');
+        listEl?.classList.remove('droppable');
+
+    }
+
+    // implement abstract method
     configure(){
+        // implement drag events
+        this.element.addEventListener('dragover',this.dragOverHandler);
+        this.element.addEventListener('dragleave',this.dragLeaveHandler);
+        this.element.addEventListener('drop',this.dropHandler);
+
         // register a listener function - we need to pass a function to addListener function
         //projects is received from projectState
         projectState.addListener((projects:Project[])=>{
