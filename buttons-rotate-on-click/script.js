6ff2c317848253c096931d48f68ac8ebf2eccdbe
unknown <nithin.thewebdev@gmail.com>
2022-02-09 14:50:45 +0530

    spiral buttons comments

--- buttons-rotate-on-click/script.js
+++ buttons-rotate-on-click/script.js
@@ -1,12 +1,15 @@
-var l = "4";
+var l = "2222";
 var a = ["1", "2", "3", "6", "9", "8", "7", "4"];
 var b = ["1", "2", "3", "6", "9", "8", "7", "4"];
 
 var rotate = function() {
+    // decrementing for loop
     for (var i = 7; i > 0; i--) {
         a[i] = a[i - 1];
     }
+    //set the starting position as l
     a[0] = l;
+    // set the l value as last value
     l = a[7];
     for (var i = 0; i < 8; i++) {
         console.log("b[i]",b[i]);
