81f32b2b80d5d46954d7fd4e34187923873e8f09
Nithin Samuel <nithin.thewebdev@gmail.com>
2022-04-18 16:03:06 +0530

    initial commit

---
+++ chartjs-projects/color-chart-background-values/script.js
@@ -0,0 +1,40 @@
+// setup block
+const data = {
+  labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+  datasets: [
+    {
+      label: "Weekly Sales",
+      data: [18, 12, 6, 9, 12, 3, 9, 0, 0],
+      backgroundColor: [
+        "rgba(255, 26, 104, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(255, 206, 86, 0.2)",
+        "rgba(75, 192, 192, 0.2)",
+        "rgba(153, 102, 255, 0.2)",
+        "rgba(255, 159, 64, 0.2)",
+        "rgba(0, 0, 0, 0.2)",
+      ],
+      borderWidth: 1,
+      // borderColor: ["rgba(255,26,104,1)"],
+      borderWidth: 2,
+    },
+  ],
+};
+//plugin -
+
+// config block
+const config = {
+  type: "bar",
+  data: data,
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+  plugins: [],
+};
+
+// render init block
+const myChart = new Chart(document.getElementById("myChart"), config);
