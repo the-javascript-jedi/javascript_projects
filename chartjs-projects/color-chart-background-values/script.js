c98eddaf8fd5073f0a57865c29622fb54332ab10
Nithin Samuel <nithin.thewebdev@gmail.com>
2022-04-18 17:34:05 +0530

    added background color based on value

--- chartjs-projects/color-chart-background-values/script.js
+++ chartjs-projects/color-chart-background-values/script.js
@@ -1,3 +1,9 @@
+// specify the background colors
+const bgColors = [
+  "rgba(255, 26, 104, 0.2)", //red
+  "rgba(255, 206, 86, 0.2)", //yellow
+  "rgba(75, 192, 192, 0.2)", //green
+];
 // setup block
 const data = {
   labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
@@ -29,7 +35,33 @@ const statusTracker = {
       chartArea: { top, bottom, left, right, width, height },
       scales: { x, y },
     } = chart;
+    const datapoints = myChart.data.datasets[0].data;
+    const datapointsLength = datapoints.length - 1;
     ctx.save();
+    // color the background
+    drawBlocks(y.max, 10, bgColors[0]); //red
+    drawBlocks(10, 4, bgColors[1]); //yellow
+    drawBlocks(4, y.min, bgColors[2]); //green
+
+    //function to color the background
+    function drawBlocks(yStart, yEnd, color) {
+      if (
+        datapoints[datapointsLength] <= yStart &&
+        datapoints[datapointsLength] >= yEnd
+      ) {
+        ctx.fillStyle = color;
+      } else {
+        ctx.fillStyle = "transparent";
+      }
+      // ctx.fillRect(left, top, width, height);
+      ctx.fillRect(
+        left,
+        y.getPixelForValue(yStart),
+        width,
+        y.getPixelForValue(yEnd) - y.getPixelForValue(yStart)
+      );
+      ctx.restore();
+    }
     // draw line using function
     drawLines(10, "rgba(255,26,104,1)");
     drawLines(4, "rgba(255,206,86,1)");
