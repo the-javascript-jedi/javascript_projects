6e47dc51abd80a7339498d812573edf0f928d325
Nithin Samuel <nithin.thewebdev@gmail.com>
2022-05-20 13:20:48 +0530

    added label

--- chartjs-projects/coin-market-cap-series/p-1/script.js
+++ chartjs-projects/coin-market-cap-series/p-1/script.js
@@ -48,7 +48,8 @@ const data = {
       tension: 0.5,
       pointRadius: 0,
       pointHitRadius: 10,
-      hoverPointRadius: 10,
+      pointHoverRadius: 10,
+      borderWidth: 2,
     },
   ],
 };
@@ -62,6 +63,7 @@ const dottedLine = {
       chartArea: { left, right, width },
       scales: { x, y },
     } = chart;
+    const startingPoint = data.datasets[0].data[0];
     ctx.save();
     ctx.beginPath();
     //ctx.lineWidth- thickness of line -1pixel
@@ -71,14 +73,30 @@ const dottedLine = {
     // color of line
     ctx.strokeStyle = "rgba(102,102,102,1)";
     // line starting point-ctx.moveTo(x,y)
-    ctx.moveTo(left, y.getPixelForValue(data.datasets[0].data[0]));
+    ctx.moveTo(left, y.getPixelForValue(startingPoint));
     // line to horizontal
-    ctx.lineTo(right, y.getPixelForValue(data.datasets[0].data[0]));
+    ctx.lineTo(right, y.getPixelForValue(startingPoint));
     // drawing command
     ctx.stroke();
     ctx.closePath();
     // to consider as a fully solid line
     ctx.setLineDash([]);
+
+    // create a new line
+    ctx.beginPath();
+    ctx.fillStyle = "rgba(102,102,102,1)";
+    ctx.fillRect(0, y.getPixelForValue(startingPoint) - 10, left, 20);
+    ctx.closePath();
+
+    // fill the label
+    // ctx,fillRect(x,y,w,h);
+    ctx.font = "12px sans-seriff";
+    ctx.fillStyle = "white";
+    ctx.textBaseline = "middle";
+    ctx.textAlign = "center";
+    // label text
+    ctx.fillText(startingPoint, left / 2, y.getPixelForValue(startingPoint));
+    //  label ends
   },
 };
 // config block
@@ -86,6 +104,11 @@ const config = {
   type: "line",
   data: data,
   options: {
+    layout: {
+      padding: {
+        left: 10,
+      },
+    },
     scales: {
       y: {
         beginAtZero: true,
