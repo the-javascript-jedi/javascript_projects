a3f7f4f8df45dd5ee0a7c442114557833c081bd7
Nithin Samuel <nithin.thewebdev@gmail.com>
2022-05-31 15:42:13 +0530

    custom tooltip added interaction

--- chartjs-projects/tooltip-multiple-data/script.js
+++ chartjs-projects/tooltip-multiple-data/script.js
@@ -5,15 +5,23 @@ const data = {
     {
       label: "Weekly Sales",
       data: [18, 12, 6, 9, 12, 3, 9],
-      backgroundColor: [
-        "rgba(255, 26, 104, 0.2)",
-        "rgba(54, 162, 235, 0.2)",
-        "rgba(255, 206, 86, 0.2)",
-        "rgba(75, 192, 192, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(255, 159, 64, 0.2)",
-        "rgba(0, 0, 0, 0.2)",
-      ],
+      backgroundColor: ["rgba(255, 26, 104, 0.2)"],
+      borderWidth: 1,
+      // borderColor: ["rgba(255,26,104,1)"],
+      borderWidth: 2,
+    },
+    {
+      label: "Weekly orders",
+      data: [18, 12, 6, 9, 12, 3, 9],
+      backgroundColor: ["rgba(54, 162, 235, 0.2)"],
+      borderWidth: 1,
+      // borderColor: ["rgba(255,26,104,1)"],
+      borderWidth: 2,
+    },
+    {
+      label: "Weekly cost",
+      data: [18, 12, 6, 9, 12, 3, 9],
+      backgroundColor: ["rgba(255, 206, 86, 0.2)"],
       borderWidth: 1,
       // borderColor: ["rgba(255,26,104,1)"],
       borderWidth: 2,
@@ -27,6 +35,10 @@ const config = {
   type: "bar",
   data: data,
   options: {
+    // Tooltip Interaction to show all the data on hover
+    interaction: {
+      mode: "index",
+    },
     scales: {
       y: {
         beginAtZero: true,
