e5791be6171d128ca47d7c508eb3e556d6490fb7
Nithin Samuel <nithin.thewebdev@gmail.com>
2022-05-31 16:44:37 +0530

    added custom legend

--- chartjs-projects/custom-legend/script.js
+++ chartjs-projects/custom-legend/script.js
@@ -3,37 +3,90 @@ const data = {
   labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
   datasets: [
     {
-      label: "Weekly Sales",
+      label: "Red Sales",
       data: [18, 12, 6, 9, 12, 3, 9],
-      backgroundColor: [
-        "rgba(255, 26, 104, 0.2)",
-        "rgba(54, 162, 235, 0.2)",
-        "rgba(255, 206, 86, 0.2)",
-        "rgba(75, 192, 192, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(255, 159, 64, 0.2)",
-        "rgba(0, 0, 0, 0.2)",
-      ],
+      backgroundColor: ["rgba(255, 26, 104, 0.2)"],
       borderWidth: 1,
-      // borderColor: ["rgba(255,26,104,1)"],
+      borderColor: ["rgba(255,26,104,1)"],
       borderWidth: 2,
+      tension: 0.3,
+    },
+    {
+      label: "Blue Sales",
+      data: [9, 15, 12, 6, 15, 9, 6],
+      backgroundColor: ["rgba(54, 162, 235, 0.2)"],
+      borderWidth: 1,
+      borderColor: ["rgba(54,162,235,1)"],
+      borderWidth: 2,
+      tension: 0.3,
     },
   ],
 };
-//plugin -
+//plugin -customLegend
+const customLegend = {
+  id: "customLegend",
+  afterDraw: (chart, args, pluginOptions) => {
+    const {
+      ctx,
+      data,
+      chartArea: { left, right, top, bottom, width, height },
+      scales: { x, y },
+    } = chart;
+    // ctx.save();
+    //  ctx.font = "bolder 12px sans-seriff";
+    //  ctx.fillStyle = data.datasets[0].borderColor;
+    //  ctx.textAlign = "right";
+    //  // ctx.fillText("Hello", left, y);
+    //  // getDatasetMeta to get index of 0 dataset 1 since two datasets are there
+    //  // chart.getDatasetMeta(0).data[0] - gets the chart data
+    //  ctx.fillText(
+    //    data.datasets[0].label,
+    //    left - 25,
+    //    chart.getDatasetMeta(0).data[0].y
+    //  );
+    // dynamically display
+    data.datasets.forEach((dataset, index) => {
+      let color = "transparent";
+      // on click of legend dataset is hidden so we will also hide the label
+      if (chart.isDatasetVisible(index) === true) {
+        color = dataset.borderColor;
+      }
+      ctx.font = "bolder 12px sans-seriff";
+      ctx.fillStyle = data.datasets[index].borderColor;
+      ctx.textAlign = "right";
 
+      ctx.fillText(
+        dataset.label,
+        left - 25,
+        chart.getDatasetMeta(index).data[0].y
+      );
+    });
+    ctx.restore();
+  },
+};
 // config block
 const config = {
-  type: "bar",
+  type: "line",
   data: data,
   options: {
+    layout: {
+      padding: {
+        // left: 25,
+        left: (context) => {
+          console.log("context", context);
+          // get the first value text label
+          const labelText = context.chart.data.datasets[0].label;
+          return context.chart.ctx.measureText(labelText).width + 15;
+        },
+      },
+    },
     scales: {
       y: {
         beginAtZero: true,
       },
     },
   },
-  plugins: [],
+  plugins: [customLegend],
 };
 
 // render init block
