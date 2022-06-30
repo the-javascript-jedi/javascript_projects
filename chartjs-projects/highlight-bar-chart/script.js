8cc63f7769aea5eda572e4c5d80d2c0074ee204b
Nithin Samuel <nithin.thewebdev@gmail.com>
2022-06-30 15:08:19 +0530

    added color for chart

--- chartjs-projects/highlight-bar-chart/script.js
+++ chartjs-projects/highlight-bar-chart/script.js
@@ -15,20 +15,55 @@ const data = {
         "rgba(0, 0, 0, 0.2)",
       ],
       borderWidth: 1,
-      // borderColor: ["rgba(255,26,104,1)"],
+      borderColor: [
+        "rgba(255, 26, 104, 0.5)",
+        "rgba(54, 162, 235, 0.5)",
+        "rgba(255, 206, 86, 0.5)",
+        "rgba(75, 192, 192, 0.5)",
+        "rgba(153, 102, 255, 0.5)",
+        "rgba(255, 159, 64, 0.5)",
+        "rgba(0, 0, 0, 0.5)",
+      ],
       borderWidth: 2,
       // bar width
-      barPercentage: 0.5,
+      // barPercentage: 0.5,
     },
   ],
 };
-//plugin -
-
+//plugin - highlightBar
+const highlightBar = {
+  id: "highlightBar",
+  afterDatasetsDraw(chart) {
+    const vote = document.getElementById("vote");
+    console.log("dropdown value", vote.value);
+    const index = chart.data.labels.indexOf(vote.value);
+    if (index >= 0) {
+      const { ctx } = chart;
+      // calculate x and y coordinate
+      const x = chart.getDatasetMeta(0).data[index].x;
+      const y = chart.getDatasetMeta(0).data[index].y;
+      // angle half circle
+      const angle = Math.PI / 180;
+      ctx.beginPath();
+      // color of ball
+      ctx.fillStyle = chart.data.datasets[0].borderColor[index];
+      // ctx.arc(x,y,radius,angleForStarting,angleForEnding,counterclockwise)
+      ctx.arc(x, y, 10, angle * 0, angle * 360, false);
+      ctx.fill();
+      ctx.closePath();
+    }
+  },
+};
 // config block
 const config = {
   type: "bar",
   data: data,
   options: {
+    plugins: {
+      tooltip: {
+        enabled: false,
+      },
+    },
     scales: {
       // aligns the grid on the grid line when false
       x: {
@@ -41,8 +76,13 @@ const config = {
       },
     },
   },
-  plugins: [],
+  plugins: [highlightBar],
 };
-
+// selected vote dropdown
+function selectedVote(vote) {
+  const index = myChart.data.labels.indexOf(vote.value);
+  console.log("dropdown index", index);
+  myChart.update();
+}
 // render init block
 const myChart = new Chart(document.getElementById("myChart"), config);
