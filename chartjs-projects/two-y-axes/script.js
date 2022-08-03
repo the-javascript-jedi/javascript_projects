a8ca61b6e294f568a3041dc52f6d832cdf69ff55
Nithin Samuel <nithin.thewebdev@gmail.com>
2022-08-03 08:45:59 +0530

    added two labels

--- chartjs-projects/two-y-axes/script.js
+++ chartjs-projects/two-y-axes/script.js
@@ -5,20 +5,26 @@ const data = {
     {
       label: "Weekly Sales",
       data: [18, 12, 6, 9, 12, 3, 9],
-      backgroundColor: [
-        "rgba(255, 26, 104, 0.2)",
-        "rgba(54, 162, 235, 0.2)",
-        "rgba(255, 206, 86, 0.2)",
-        "rgba(75, 192, 192, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(255, 159, 64, 0.2)",
-        "rgba(0, 0, 0, 0.2)",
-      ],
+      backgroundColor: ["rgba(255, 26, 104, 0.2)"],
+      borderColor: ["rgba(255, 26, 104, 0.2)"],
       borderWidth: 1,
       // borderColor: ["rgba(255,26,104,1)"],
       borderWidth: 2,
       // bar width
       barPercentage: 0.5,
+      yAxisID: "currency",
+    },
+    {
+      label: "Weekly Cost",
+      data: [9, 9, 6, 9, 9, 9, 100],
+      backgroundColor: ["rgba(0, 0, 0, 0.2)"],
+      borderColor: ["rgba(0, 0, 0, 0.2)"],
+      borderWidth: 1,
+      // borderColor: ["rgba(255,26,104,1)"],
+      borderWidth: 2,
+      // bar width
+      barPercentage: 0.5,
+      yAxisID: "percentage",
     },
   ],
 };
@@ -30,16 +36,22 @@ const config = {
   data: data,
   options: {
     scales: {
-      // aligns the grid on the grid line when false
-      x: {
-        grid: {
-          offset: true,
-          // dont-display-grid lines
-          display: true,
-        },
+      // custom y axis left
+      currency: {
+        type: "linear",
+        position: "left",
+        // min to be shown
+        min: 2,
+        max: 10,
       },
-      y: {
-        beginAtZero: true,
+      // custom y axis right
+      percentage: {
+        type: "linear",
+        position: "right",
+        // remove grid lines of right y axis
+        grid: {
+          display: false,
+        },
       },
     },
   },
