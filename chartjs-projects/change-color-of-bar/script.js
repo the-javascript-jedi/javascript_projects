3023bc45ef064f22919e3bfd7b63ca44d70b6d7b
Nithin Samuel <nithin.thewebdev@gmail.com>
2022-09-02 08:15:08 +0530

    added color for background bar chart

---
+++ chartjs-projects/change-color-of-bar/script.js
@@ -0,0 +1,62 @@
+// setup block
+const data = {
+  labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+  datasets: [
+    {
+      label: "Weekly Sales",
+      data: [18, 12, 6, 9, 12, 3, 9],
+      // backgroundColor: [
+      //   "rgba(255, 26, 104, 0.2)",
+      //   "rgba(54, 162, 235, 0.2)",
+      //   "rgba(255, 206, 86, 0.2)",
+      //   "rgba(75, 192, 192, 0.2)",
+      //   "rgba(153, 102, 255, 0.2)",
+      //   "rgba(255, 159, 64, 0.2)",
+      //   "rgba(0, 0, 0, 0.2)",
+      // ],
+      backgroundColor: (color) => {
+        console.log("color", color);
+        let colors =
+          color.index > 3 ? "rgba(255, 26, 104, 1)" : "rgba(54, 162, 235, 1)";
+        return colors;
+      },
+      borderColor: (color) => {
+        console.log("color", color);
+        let colors =
+          color.index > 3 ? "rgba(255, 26, 104, 1)" : "rgba(54, 162, 235, 1)";
+        return colors;
+      },
+      borderWidth: 1,
+      // borderColor: ["rgba(255,26,104,1)"],
+      borderWidth: 2,
+      // bar width
+      barPercentage: 0.5,
+    },
+  ],
+};
+//plugin -
+
+// config block
+const config = {
+  type: "bar",
+  data: data,
+  options: {
+    scales: {
+      // aligns the grid on the grid line when false
+      x: {
+        grid: {
+          offset: true,
+          // dont-display-grid lines
+          display: true,
+        },
+      },
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+  plugins: [],
+};
+
+// render init block
+const myChart = new Chart(document.getElementById("myChart"), config);
