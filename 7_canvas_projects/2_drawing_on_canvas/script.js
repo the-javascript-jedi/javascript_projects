a747e4ff7fc907a34c237ba0b6018f110d478595
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-08-15 20:43:33 +0530

    drawing on canvas

--- 7_canvas_projects/2_drawing_on_canvas/script.js
+++ 7_canvas_projects/2_drawing_on_canvas/script.js
@@ -5,7 +5,7 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 var c = canvas.getContext("2d");
-
+// c.fillRect(x, y, width, height);
 c.fillRect(100, 100, 100, 100);
 c.fillRect(400, 100, 100, 100);
 c.fillRect(300, 300, 100, 100);
