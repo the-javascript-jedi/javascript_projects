3f0f66428d4b75a6736b19003b9d271aae429f34
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-09-12 21:09:25 +0530

    p5 intro

---
+++ 8_p5js/1_intro/script.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  // draw an ellipse
+  ellipse(50, 50, 80, 80);
+  // draw a rectangle - x,y,width,height
+  rect(80, 50, 20, 20);
+  //draw a line - x1,y1,x2,y2
+  line(200, 50, 300, 92);
+}
