def44e8a38ed43494d0923defdd7e047c8179981
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-09-13 23:07:48 +0530

    added ball movement

--- 8_p5js/3_variables/script.js
+++ 8_p5js/3_variables/script.js
@@ -1,20 +1,41 @@
-// Painting App
+// // Painting App
+// function setup() {
+//   createCanvas(600, 400);
+// background is not redrawn as setup block is executed only once
+//   background(250, 250, 100);
+// }
+// //mouseX - point where mouse is
+// function draw() {
+//   // background
+
+//   // ellipse
+//   noStroke();
+//   fill(250, 200, 200, 50);
+//   ellipse(mouseX, mouseY, 100, 100);
+
+//   // // rectangle
+//   // fill(200, 250, 200);
+//   // rect(400, mouseY, 50, 50);
+// }
+// // click event
+// function mousePressed() {
+//   background(250, 250, 100);
+// }
+
+// Move the circle
+var circleX = 0;
 function setup() {
   createCanvas(600, 400);
-  background(250, 250, 100);
 }
 //mouseX - point where mouse is
 function draw() {
   // background
-
+  background(250, 250, 100);
   // ellipse
-  noStroke();
   fill(250, 200, 200, 50);
-  ellipse(mouseX, mouseY, 100, 100);
-
-  // // rectangle
-  // fill(200, 250, 200);
-  // rect(400, mouseY, 50, 50);
+  ellipse(circleX, mouseY, 100, 100);
+  // move the circle by 1 n the x position
+  circleX = circleX + 1;
 }
 // click event
 function mousePressed() {
