8c91f13bed1d32bd381fc408646c512c037c5e9f
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-09-17 19:58:51 +0530

    added for nested loop

--- 8_p5js/the-coding-train/8_while_for_loop/script.js
+++ 8_p5js/the-coding-train/8_while_for_loop/script.js
@@ -8,9 +8,23 @@ function draw() {
   stroke(255);
 
   var x = 0;
+  // draw single row ellipse using while loop
+  // while (x <= width) {
+  //   fill(0, 200, 255);
+  //   ellipse(x, 200, 25, 25);
+  //   x = x + 50;
+  // }
+  // draw single row ellipse using for loop
+  // for (var x = 0; x <= innerWidth; x = x + 50) {
+  //   fill(random(255), 0, 200);
+  //   ellipse(x, 300, 25, 25);
+  // }
 
-  while (x <= width) {
-    ellipse(x, 200, 25, 25);
-    x = x + 50;
+  // fill the page with blinking circles
+  for (var x = 0; x <= width; x += 50) {
+    for (var y = 0; y <= width; y += 50) {
+      fill(random(255), 0, random(255));
+      ellipse(x, y, 25, 25);
+    }
   }
 }
