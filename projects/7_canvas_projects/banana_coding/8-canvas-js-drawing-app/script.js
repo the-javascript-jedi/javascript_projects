92d4169de1487829ca98e36a73d7218dbacc8866
Nithin Samuel <nithin.thewebdev@gmail.com>
2024-03-01 08:36:41 +0530

    added canvas drawing

---
+++ projects/7_canvas_projects/banana_coding/8-canvas-js-drawing-app/script.js
@@ -0,0 +1,62 @@
+console.log("script loaded");
+const canvas = document.getElementById("canvas");
+// initialize canvas width and height
+canvas.width = window.innerWidth - 60;
+canvas.height = 400;
+
+let context = canvas.getContext("2d");
+// fill canvas with white color
+context.fillStyle = "white";
+context.fillRect(0, 0, canvas.width, canvas.height);
+
+let draw_color = "black";
+let draw_width = "2";
+let is_drawing = false;
+
+// events - starting and drawing
+// mobile/ipad events
+canvas.addEventListener("touchstart", start, false);
+canvas.addEventListener("touchmove", draw, false);
+// pc events
+canvas.addEventListener("mousedown", start, false);
+canvas.addEventListener("mousemove", draw, false);
+// events - stoping
+canvas.addEventListener("touchend", stop, false);
+canvas.addEventListener("mouseup", stop, false);
+canvas.addEventListener("mouseout", stop, false);
+
+// canvas start beginpath
+function start(event) {
+  is_drawing = true;
+  context.beginPath();
+  context.moveTo(
+    event.clientX - canvas.offsetLeft,
+    event.clientY - canvas.offsetTop
+  );
+  event.preventDefault();
+}
+
+// drawing
+function draw(event) {
+  if (is_drawing) {
+    context.lineTo(
+      event.clientX - canvas.offsetLeft,
+      event.clientY - canvas.offsetTop
+    );
+    context.strokeStyle = draw_color;
+    context.lineWidth = draw_width;
+    context.lineCap = "round";
+    context.lineJoin = "round";
+    context.stroke();
+  }
+}
+
+// canvas closepath
+function stop(event) {
+  if (is_drawing) {
+    context.stroke();
+    context.closePath();
+    is_drawing = false;
+  }
+  event.preventDefault();
+}
