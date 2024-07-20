7b17feec3f7ea44c0c8dae053c279f40ddf86107
Nithin Samuel <nithin.thewebdev@gmail.com>
2024-07-20 15:58:39 +0530

    char count max

--- projects/2_strings exercise/string-projects/commonly-used-character.js
+++ projects/2_strings exercise/string-projects/commonly-used-character.js
@@ -1,14 +1,25 @@
 console.log("commonly-used-character loaded");
-let stringTest = "abcccccccd";
+let stringTest = "nithin";
 
 const charMap = {};
-
-for (let char of stringTest) {
-  if (charMap[char]) {
-    charMap[char]++;
+// create object with count of string values
+for (let stringVal of stringTest) {
+  if (charMap[stringVal]) {
+    charMap[stringVal]++;
   } else {
-    charMap[char] = 1;
+    charMap[stringVal] = 1;
+  }
+}
+let max = 0;
+let maxChar = "";
+// find the max value
+for (let char in charMap) {
+  if (charMap[char] > max) {
+    max = charMap[char];
+    maxChar = char;
   }
 }
 
 console.log("charMap", charMap);
+console.log("max", max);
+console.log("maxChar", maxChar);
