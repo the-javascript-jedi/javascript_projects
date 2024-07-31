5db8afc0a9eb19c47b8bb8c50e609f435c64828e
Nithin Samuel <nithin.thewebdev@gmail.com>
2024-07-31 16:35:35 +0530

    working on vowel search

--- projects/2_strings exercise/string-projects/sandbox.js
+++ projects/2_strings exercise/string-projects/sandbox.js
@@ -1,14 +1,34 @@
-const myPromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    reject(new Error("Blah Blah Blah"));
-    console.log("Promise called");
-  }, 3000);
-});
+function findVowels(str) {
+  // let letters = str.split("");
+  // console.log("letters", letters);
 
-myPromise
-  .then((result) => {
-    console.log("myPromise called", result);
-  })
-  .catch((error) => {
-    console.log("error", error);
+  let vowels = ["a", "e", "i", "o", "u"];
+  let obj = {};
+  str.split("").forEach((val) => {
+    // debugger;
+    if (obj[val]) {
+      obj[val] = obj[val] + 1;
+    } else {
+      obj[val] = 1;
+    }
   });
+  console.log("obj", obj);
+}
+
+findVowels("Hi There"); //3
+
+// function findVowelsCount(str) {
+//     const vowels = ['a', 'e', 'i', 'o', 'u'];
+//     let count = 0;
+
+//     for (let char of str.toLowerCase()) {
+//         if (vowels.includes(char)) {
+//             count++;
+//         }
+//     }
+
+//     return count;
+// }
+
+// // Example usage
+// console.log(findVowelsCount("Hi There")); // Output: 3
