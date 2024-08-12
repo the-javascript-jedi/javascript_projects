5cccb9342be4a2146325e1fd7b3e195eeea58d11
Nithin Samuel <nithin.thewebdev@gmail.com>
2024-08-12 16:46:59 +0530

    added text splitting

--- projects/2_strings exercise/string-projects/sandbox.js
+++ projects/2_strings exercise/string-projects/sandbox.js
@@ -1,13 +1,17 @@
-const promise1 = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("Promise 1 Resolved");
-  }, 200);
+const users = ["ns1234", "winu4321", "rian8524"];
+
+const result = users.map((user) => {
+  const name = user.match(/[a-zA-Z]+/)[0]; // Extract the alphabetic part
+  console.log("name", name);
+  // The 10 in parseInt(user.match(/\d+/)[0], 10) specifies the radix or base for the number conversion.
+  const id = parseInt(user.match(/\d+/)[0], 10); // Extract the numeric part and convert to an integer
+  return { name, id };
 });
 
-const promise2 = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("Promise 2 Resolved");
-  }, 3000);
-});
-
-Promise.allSettled([promise1, promise2]).then((val) => console.log(val));
+console.log(result);
+// O/P
+// [
+//     {"name": "ns","id": 1234 },
+//     {"name": "winu","id": 4321},
+//     {"name": "rian","id": 8524}
+// ]
