884537b87b254ba10212b8266f8136b303ebef24
Nithin Samuel <nithin.thewebdev@gmail.com>
2024-08-14 12:15:09 +0530

    addded bubble sort

--- projects/2_strings exercise/string-projects/sandbox.js
+++ projects/2_strings exercise/string-projects/sandbox.js
@@ -1,14 +1,55 @@
-const person = {
-  name: "testUser",
-};
-function greet(greeting) {
-  console.log(greeting + " " + this.name);
+let nestedArr = [1, 2, 3, [4, 5, [6, 7, 8]]];
+function flattenNestedArr(arrSource) {
+  let resultArr = [];
+  function flattenArr(innerArr) {
+    for (let i = 0; i < innerArr.length; i++) {
+      if (Array.isArray(innerArr[i])) {
+        flattenArr(innerArr[i]);
+      } else {
+        resultArr.push(innerArr[i]);
+      }
+    }
+  }
+  flattenArr(arrSource);
+  return resultArr;
 }
-//call
-greet.call(person, "hello call");
+console.log(flattenNestedArr(nestedArr));
+/////////////////////////////////////////////////////////////////////////////////////
+/*
+function bubbleSort(array) {
+  const length = array.length;
 
-//apply
-greet.apply(person, ["hello apply"]);
+  for (let i = 0; i < length - 1; i++) {
+    for (let j = 0; j < length - i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        // Swap elements
+        let temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
 
-let bindFunc = greet.bind(person, "hello bind");
-bindFunc();
+  return array;
+}
+*/
+function bubbleSort(arr) {
+  let n = arr.length;
+  for (let i = 0; i < n - 1; i++) {
+    // Last i elements are already in place
+    for (let j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        // Swap the elements
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+// Example usage:
+let array = [64, 34, 25, 12, 22, 11, 90];
+console.log("Sorted array:", bubbleSort(array));
+// O/P Sorted array: (7) [11, 12, 22, 25, 34, 64, 90]
