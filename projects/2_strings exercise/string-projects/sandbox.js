757754fbbedbc94db0504da4872aa4a18a158419
Nithin Samuel <nithin.thewebdev@gmail.com>
2024-08-19 23:07:39 +0530

    commmented bubble sort

--- projects/2_strings exercise/string-projects/sandbox.js
+++ projects/2_strings exercise/string-projects/sandbox.js
@@ -33,23 +33,23 @@ function bubbleSort(array) {
   return array;
 }
 */
-function bubbleSort(arr) {
-  let n = arr.length;
-  for (let i = 0; i < n - 1; i++) {
-    // Last i elements are already in place
-    for (let j = 0; j < n - i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        // Swap the elements
-        let temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
-      }
-    }
-  }
-  return arr;
-}
+// function bubbleSort(arr) {
+//   let n = arr.length;
+//   for (let i = 0; i < n - 1; i++) {
+//     // Last i elements are already in place
+//     for (let j = 0; j < n - i - 1; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         // Swap the elements
+//         let temp = arr[j];
+//         arr[j] = arr[j + 1];
+//         arr[j + 1] = temp;
+//       }
+//     }
+//   }
+//   return arr;
+// }
 
-// Example usage:
-let array = [64, 34, 25, 12, 22, 11, 90];
-console.log("Sorted array:", bubbleSort(array));
-// O/P Sorted array: (7) [11, 12, 22, 25, 34, 64, 90]
+// // Example usage:
+// let array = [64, 34, 25, 12, 22, 11, 90];
+// console.log("Sorted array:", bubbleSort(array));
+// // O/P Sorted array: (7) [11, 12, 22, 25, 34, 64, 90]
