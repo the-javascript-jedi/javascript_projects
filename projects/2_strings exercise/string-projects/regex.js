cf47d5c214c63443647622abfc31cad90d5be4ec
Nithin Samuel <nithin.thewebdev@gmail.com>
2024-10-08 19:22:02 +0530

    added solution for iban regex 2

--- projects/2_strings exercise/string-projects/regex.js
+++ projects/2_strings exercise/string-projects/regex.js
@@ -8,14 +8,14 @@
 // console.log(validateIbanCode("INVALIDCODE")); // returns false
 // console.log(validateIbanCode("fr12 3456 7890 1234 56789012 345")); // returns true
 function validateIbanCodeNoSpaces(input) {
-  const regex =
-    /((FR|Fr|FR|fr)\s[0-9]{1}\s([0-9]{4}\s){5}[0-9]{1})|((AT|At|aT|at)\d{18})$/;
+  //   const regex = /((FR|Fr|FR|fr)\s[0-9]{1}\s([0-9]{4}\s){5}[0-9]{1})|((AT|At|aT|at)\d{18})$/;
+  const regex = /^((FR|Fr|FR|fr)\s[0-9]{1}\s([0-9]{4}\s){5}[0-9]{4})$/;
   return regex.test(input);
 }
 console.log(validateIbanCodeNoSpaces("FR 1 2345 6789 0123 4567 8901 2345")); // returns true
 
-function regexStringCheck(input) {
-  const regex = /^[a-zA-Z]{5}$/;
-  return regex.test(input);
-}
-console.log(regexStringCheck("abcde"));
+// function regexStringCheck(input) {
+//   const regex = /^[a-zA-Z]{5}$/;
+//   return regex.test(input);
+// }
+// console.log(regexStringCheck("abcde"));
