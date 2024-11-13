05bedcbb525bf211841236e84ffbbb1f82140999
Nithin Samuel <nithin.thewebdev@gmail.com>
2024-11-13 10:05:34 +0530

    added anagrams

--- projects/2_strings exercise/string-projects/sandbox.js
+++ projects/2_strings exercise/string-projects/sandbox.js
@@ -1,23 +1,10 @@
-function lengthOfLongestSubstring(str) {
-  let maxLength = 0;
-  for (let i = 0; i < str.length; i++) {
-    let substring = "";
-    for (let j = i; j < str.length; j++) {
-      const currentChar = str[j];
-      if (substring.includes(currentChar)) {
-        break;
-      }
-      substring = substring + currentChar;
-      if (substring.length > maxLength) {
-        maxLength = substring.length;
-      }
-    }
-    console.log("substring", substring);
-    return maxLength;
-  }
+function isAnagram(s, t) {
+  let first = s.split("").sort().join("");
+  let second = t.split("").sort().join("");
+  return first == second;
+  console.log("first", first);
+  console.log("second", second);
 }
 
 // Example usage:
-console.log(lengthOfLongestSubstring("abcabcbb")); // Output: 3
-// console.log(lengthOfLongestSubstring("bbbbb")); // Output: 1
-// console.log(lengthOfLongestSubstring("pwwkew")); // Output: 3
+console.log(isAnagram("car", "rac")); // Output: 3
