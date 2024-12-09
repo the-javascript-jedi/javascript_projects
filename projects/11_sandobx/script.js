105fbbc4d29c4b6d84c5b0c89538add2b7d964e4
Nithin Samuel <nithin.thewebdev@gmail.com>
2024-12-09 20:00:07 +0530

    added solution for traversal

--- projects/11_sandobx/script.js
+++ projects/11_sandobx/script.js
@@ -1,8 +1,55 @@
-function twoSum(nums, target) {
-  // Your code here
-  let result = [];
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = 0; j < nums.length; j++) {}
+// console.log("data", data);
+console.log("_", _);
+let transactions = data.transaction;
+// let displayTransactions = transactions.splice(0, 1);
+// console.log("displayTransactions", displayTransactions);
+// Assuming `transactions` is your array of transactions
+const groupedTransactions = _.groupBy(
+  transactions,
+  (transaction) => transaction.receiver.uid
+);
+console.log("groupedTransactions", groupedTransactions);
+
+const formattedData = Object.entries(groupedTransactions).map(
+  ([uid, transactions]) => {
+    console.log("uid, transactions", uid, transactions);
+    // Calculate total received amount and transfer fee
+    const totalReceivedAmount = transactions.reduce(
+      (sum, transaction) =>
+        sum +
+        (transaction.payment_details.destination.actual_payout_amount || 0),
+      0
+    );
+
+    const totalTransferFee = transactions.reduce(
+      (sum, transaction) =>
+        sum + (transaction.payment_details.fees.charges || 0),
+      0
+    );
+
+    // Build the formatted object for each receiver
+    return {
+      receiverName:
+        transactions[0].receiver.name.first_name +
+        " " +
+        transactions[0].receiver.name.last_name,
+      receiverUid: uid,
+      totalReceivedAmount: totalReceivedAmount.toString(),
+      totalTransferFee: totalTransferFee.toString(),
+      transactionDetails: transactions.map((transaction) => ({
+        fillingDate: transaction.filing_date,
+        receiverCorridor: transaction.receiver.address.country_iso_code || "",
+        deliveryMethod:
+          transaction.payment_details.payment_method.payment_type || "",
+        transactionId: transaction.money_transfer_control.mtcn || "",
+        sendAmount:
+          transaction.payment_details.origination.principal_amount || "",
+        transferFee: transaction.payment_details.fees.charges || "",
+        amountReceived:
+          transaction.payment_details.destination.actual_payout_amount || "",
+      })),
+    };
   }
-}
-console.log(twoSum([2, 7, 11, 15], 9)); // [0, 1]
+);
+console.log(formattedData);
+//////////////////////////////////////////////////////////////////////////////
