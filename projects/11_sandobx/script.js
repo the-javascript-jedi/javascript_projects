14b684d31695102ca32ee620db8deecec389fb72
Nithin Samuel <nithin.thewebdev@gmail.com>
2024-12-25 21:08:09 +0530

    added fib

--- projects/11_sandobx/script.js
+++ projects/11_sandobx/script.js
@@ -1,32 +1,12 @@
-function findTwoNumbers(nums, target) {
-  let result = [];
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] === target) {
-        result.push(nums[i], nums[j]);
-      }
-    }
+function fibonacci(n) {
+  let fibSequence = [0, 1];
+  for (let i = 2; i < n; i++) {
+    fibSequence.push(fibSequence[i - 2] + fibSequence[i - 1]);
   }
-  return result;
+  return fibSequence;
 }
 
-// Example Usage
-console.log(findTwoNumbers([1, 3, 4, 5, 6], 10)); // Output: [2, 7]
-
-//////////////////////////////////////////////////////////////////
-// function findTwoNumbers(nums, target) {
-//   for (var i = 0; i < nums.length; i++) {
-//     for (var j = i + 1; j < nums.length; j++) {
-//       if (nums[i] + nums[j] === target) {
-//         return [nums[i], nums[j]]; // Return the two numbers
-//       }
-//     }
-//   }
-//   return null; // Return null if no such pair is found
-// }
-
-// // Example Usage
-console.log(findTwoNumbers([2, 7, 11, 15], 9)); // Output: [2, 7]
-console.log(findTwoNumbers([1, 3, 4, 5, 6], 10)); // Output: [4, 6]
-console.log(findTwoNumbers([5, 1, 2, 8], 7)); // Output: [5, 2]
-console.log(findTwoNumbers([1, 2, 3], 7)); // Output: null
+// Example usage:
+console.log(fibonacci(5)); // Output: 5
+console.log(fibonacci(10)); // Output: 55
+console.log(fibonacci(20)); // Output: 6765
