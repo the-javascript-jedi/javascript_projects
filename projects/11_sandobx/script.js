ff06174af1c042c7febbe8b8736d8775fa678eb1
Nithin Samuel <nithin.thewebdev@gmail.com>
2025-01-04 19:59:27 +0530

    added solution

--- projects/11_sandobx/script.js
+++ projects/11_sandobx/script.js
@@ -1,28 +1,16 @@
-function mostFrequentElement(arr) {
-  let numCount = {};
-  let maxCount = 0;
-  let maxElement = "";
-  arr.forEach((val) => {
-    console.log(val);
-    if (numCount[val]) {
-      numCount[val] = numCount[val] + 1;
-    } else {
-      numCount[val] = 1;
-    }
-  });
-
-  for (let key in numCount) {
-    console.log("key", key);
-    if (numCount[key] > maxCount) {
-      maxCount = numCount[key];
-      maxElement = key;
+function twoSum(nums, target) {
+  // console.log("nums", nums);
+  // console.log("target", target);
+  let sum = 0;
+  let resultArr = [];
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i; j < nums.length; j++) {
+      if (nums[i] + nums[j] == target) {
+        resultArr.push([i, j]);
+      }
     }
   }
-  console.log("numCount", numCount);
-  console.log("maxCount", maxCount);
-  console.log("maxElement", maxElement);
+  console.log("resultArr", resultArr);
 }
 
-// Test
-var arr = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4];
-console.log(mostFrequentElement(arr)); // 4
+console.log(twoSum([2, 7, 11, 15], (target = 9)));
