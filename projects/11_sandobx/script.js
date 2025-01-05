b63e8a86cf95ca43cb8da7e6608488acd342bf5a
Nithin Samuel <nithin.thewebdev@gmail.com>
2025-01-05 19:47:27 +0530

    added solution

--- projects/11_sandobx/script.js
+++ projects/11_sandobx/script.js
@@ -1,41 +1,16 @@
-function lengthOfLongestSubstring(str) {
-  // Handle empty string
-  if (!str) {
-    return 0;
-  }
-
-  var maxLength = 0;
-  var start = 0;
-  var charMap = {};
-
-  for (var i = 0; i < str.length; i++) {
-    var currentChar = str[i];
-
-    // If we've seen this character before and its index is >= start
-    if (charMap[currentChar] >= start) {
-      // Move start to the position after the previous occurrence
-      start = charMap[currentChar] + 1;
-    }
-
-    // Store the current character's position
-    charMap[currentChar] = i;
-
-    // Update maxLength if current substring is longer
-    var currentLength = i - start + 1;
-    if (currentLength > maxLength) {
-      maxLength = currentLength;
+function lengthOfLongestSubstring(s) {
+  let maxlength = 0;
+  for (let i = 0; i < s.length; i++) {
+    for (let j = i; j < s.length; j++) {
+      let substring = s.slice(i, j + 1);
+      console.log("substring", substring);
+      if (substring.indexOf(s[j]) !== substring.lastIndexOf(s[j])) {
+        break;
+      }
+      maxlength = Math.max(maxlength, substring.length);
     }
   }
-
-  return maxLength;
+  return maxlength;
 }
 
-// Test cases
 console.log(lengthOfLongestSubstring("abcabcbb")); // Output: 3
-console.log(lengthOfLongestSubstring("bbbbb")); // Output: 1
-console.log(lengthOfLongestSubstring("pwwkew")); // Output: 3
-console.log(lengthOfLongestSubstring("")); // Output: 0
-console.log(lengthOfLongestSubstring("au")); // Output: 2
-
-
-
