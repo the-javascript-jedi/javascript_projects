9c8dece996e4e11e432f1e4c426e6f1789807552
Nithin Samuel <nithin.thewebdev@gmail.com>
2025-02-17 23:15:24 +0530

    added day 1 advent

--- projects/11_sandobx/script.js
+++ projects/11_sandobx/script.js
@@ -1,117 +1,18 @@
-import {
-  waitForAsync,
-  ComponentFixture,
-  TestBed,
-  inject,
-} from "@angular/core/testing";
-import { Component, EventEmitter } from "@angular/core";
+let x = [3, 4, 2, 1, 3, 3];
+let y = [4, 3, 5, 3, 9, 3];
 
-import { RemittanceStatementPdfComponent } from "./remittance-statement-pdf";
-import { DownloadPdfService } from "./../../../sand-box/download-pdf.service";
+let sortedX = [...x].sort((a, b) => a - b);
+let sortedY = [...y].sort((a, b) => a - b);
 
-import dayjs from "dayjs";
-import { jsPDF } from "jspdf";
+console.log("sortedX", sortedX); // [1, 2, 3, 3, 3, 4]
+console.log("sortedY", sortedY); // [3, 3, 3, 4, 5, 9]
 
-import html2canvas from "html2canvas";
-import { SpinnerService } from "./../../../core/services";
+let distanceArr = [];
+for (let i = 0; i < sortedX.length; i++) {
+  let distance = Math.abs(sortedX[i] - sortedY[i]);
+  distanceArr.push(distance);
+}
 
-// Mock JSPDF
-jest.mock("jspdf", () => {
-  return {
-    jsPDF: jest.fn(() => ({
-      pageSize: {
-        internal: {
-          getWidth: jest.fn().mockReturnValue(210),
-          getHeight: jest.fn().mockReturnValue(297),
-        },
-      },
-      addImage: jest.fn(),
-      addPage: jest.fn(),
-      save: jest.fn(),
-    })),
-  };
-});
-
-// Mock HTML2Canvas
-jest.mock("html2canvas", () => {
-  return jest.fn().mockImplementation(() => {
-    const mockCanvas = {
-      toDataURL: jest
-        .fn()
-        .mockReturnValue("data:image/png;base64, mockImageData"),
-      width: 800,
-      height: 600,
-      getContext: jest.fn().mockReturnValue({
-        drawImage: jest.fn(),
-      }),
-    };
-    return Promise.resolve(mockCanvas);
-  });
-
-  describe("RemittanceStatementPdfComponent", () => {
-    let component: RemittanceStatementPdfComponent;
-    let fixture: ComponentFixture<RemittanceStatementPdfComponent>;
-    let spinnerService: SpinnerService;
-
-    // let jsPdfSpy: jest.SpyInstance;
-
-    const mockSpinnerService = {
-      startCall: jest.fn(),
-      endCall: jest.fn(),
-    };
-
-    // Mock Data
-    const mockRemittanceData = [
-      { pageId: "page1", data: "test1" },
-      { pageId: "page2", data: "test2" },
-    ];
-
-    beforeEach(async () => {
-      await TestBed.configureTestingModule({
-        declarations: [RemittanceStatementPdfComponent],
-        providers: [{ provide: SpinnerService, useValue: mockSpinnerService }],
-      }).compileComponents();
-    });
-
-    beforeEach(() => {
-      fixture = TestBed.createComponent(RemittanceStatementPdfComponent);
-      component = fixture.componentInstance;
-      spinnerService = TestBed.inject(SpinnerService);
-    });
-
-    // jsPdfSpy = jest.spyOn(jsPDF, 'default');
-
-    beforeEach(() => {
-      jest.clearAllMocks();
-
-      const mockElement = {
-        getBoundingClientRect: () => ({
-          width: 500,
-          height: 500,
-          top: 0,
-          left: 0,
-          right: 500,
-          bottom: 500,
-        }),
-        style: {},
-        clientWidth: 500,
-        clientHeight: 500,
-        offsetWidth: 500,
-        offsetHeight: 500,
-      };
-
-      document.getElementById = jest.fn().mockReturnValue(mockElement);
-
-      // Reset component input
-      component.remittanceStatementData = mockRemittanceData;
-      component.senderFullName = "John Doe";
-    });
-
-    describe("generateRemittanceStatement", () => {
-      it("should generate PDF with correct number of pages", async () => {
-        await component.generateRemittanceStatement();
-        expect(spinnerService.endCall).toHaveBeenCalled();
-      });
-    });
-  });
-});
+console.log("distanceArr", distanceArr); // [2, 1, 0, 1, 2, 5]
+const total = distanceArr.reduce((acc, currValue) => acc + currValue, 0);
+console.log("total", total); // 11
