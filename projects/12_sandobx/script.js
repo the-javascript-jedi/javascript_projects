e62a819db2ee70b2bac3084a2dfec1042de7f554
nithin.thewebdev <nithin.thewebdev@gmail.com>
2026-06-24 08:44:14 +0530

    added sandbox check

--- projects/12_sandobx/script.js
+++ projects/12_sandobx/script.js
@@ -1,29 +1,23 @@
-// function constructNote(val1, val2) {
-//   let freq = {};
-
-//   // count characters available in val2 (the source)
-//   for (let char of val2) {
-//     freq[char] = (freq[char] || 0) + 1;
-//   }
-
-//   // check if val1 can be built from those characters
-//   for (let char of val1) {
-//     if (!freq[char]) {
-//       // character missing or used up
-//       return false;
-//     }
-//     freq[char]--; // consume one occurrence
-//   }
-
-//   return true;
-// }
-
-// console.log(constructNote("aa", "abc")); // false
-// console.log(constructNote("abc", "dcba")); // true
-// console.log(constructNote("aabbcc", "bcabcaddff")); // true
-
-function constructNote(str1, str2) {}
-
 console.log(constructNote("aa", "abc")); // false
 console.log(constructNote("abc", "dcba")); // true
 console.log(constructNote("aabbcc", "bcabcaddff")); // true
+
+function constructNote(str1, str2) {
+  let freq2 = {};
+  for (let char of str2) {
+    if (freq2[char]) {
+      freq2[char]++;
+    } else {
+      freq2[char] = 1;
+    }
+  }
+
+  for (let char of str1) {
+    if (!freq2[char]) {
+      return false;
+    }
+    freq2[char]--;
+  }
+  console.log("freq2", freq2);
+  return true;
+}
